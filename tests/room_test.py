d6a125300cf6e8ca8de946f3ec05f33dae35d181
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2020-03-12 09:54:09 +0000

    added setUp() method to be run before each test

--- tests/room_test.py
+++ tests/room_test.py
@@ -9,46 +9,42 @@ from classes.song import Song
 
 class TestRoom(unittest.TestCase):
 
+    def setUp(self):
+        self._room = Room("The Metal Room")
+
     def test_room_has_name(self):
-        room = Room("The Metal Room")
-        self.assertEqual("The Metal Room", room.get_name())
+        self.assertEqual("The Metal Room", self._room.get_name())
 
     def test_room_has_no_guests_at_start(self):
-        room = Room("The Blues Room")
-        self.assertEqual(0, room.number_of_guests())
+        self.assertEqual(0, self._room.number_of_guests())
 
     def test_room_has_no_songs_at_start(self):
-        room = Room("The Blues Room")
-        self.assertEqual(0, room.number_of_songs())
+        self.assertEqual(0, self._room.number_of_songs())
 
     def test_can_check_in_guest(self):
-        room = Room("The Blues Room")
         guest = Guest("Victor")
-        room.check_in_guest(guest)
-        self.assertEqual(1, room.number_of_guests())
+        self._room.check_in_guest(guest)
+        self.assertEqual(1, self._room.number_of_guests())
 
     def test_can_check_in_multiple_guests(self):
-        room = Room("The Blues Room")
         jack = Guest("Jack")
         victor = Guest("Victor")
         isa = Guest("Isa")
 
         guests = [jack, victor, isa]
-        room.check_in_guests(guests)
-        self.assertEqual(3, room.number_of_guests())
+        self._room.check_in_guests(guests)
+        self.assertEqual(3, self._room.number_of_guests())
 
     def test_can_check_guest_out(self):
-        room = Room("The Blues Room")
         guest = Guest("Victor")
-        room.check_in_guest(guest)
-        room.check_out_guest(guest)
-        self.assertEqual(0, room.number_of_guests())
+        self._room.check_in_guest(guest)
+        self._room.check_out_guest(guest)
+        self.assertEqual(0, self._room.number_of_guests())
 
     def test_can_add_song_to_room(self):
-        room = Room("The Metal Room")
         song = Song("The Number of the Beast", "Iron Maiden")
-        room.add_song(song)
-        self.assertEqual(1, room.number_of_songs())
+        self._room.add_song(song)
+        self.assertEqual(1, self._room.number_of_songs())
 
 
 if __name__ == '__main__':
